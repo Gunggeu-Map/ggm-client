ff136fa771ae874ec42397db41fd0e719a2efa17
ljg0610 <jggy0627@naver.com>
2025-04-20 03:52:34 +0900

    feat: 질문,답변조회 api 추가

--- app/src/main/java/com/gunggeumap/ggm/data/remote/GgmApiService.kt
+++ app/src/main/java/com/gunggeumap/ggm/data/remote/GgmApiService.kt
@@ -4,6 +4,7 @@ import com.gunggeumap.ggm.data.model.ApiResult
 import com.gunggeumap.ggm.ui.viewmodel.dto.QuestionSummary
 import com.gunggeumap.ggm.ui.viewmodel.dto.ShortInfo
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GgmApiService {
     @GET("api/questions/top")
@@ -11,4 +12,14 @@ interface GgmApiService {
 
     @GET("api/short-infos")
     suspend fun getShortInfos(): ApiResult<List<ShortInfo>>
+
+    @GET("api/questions/{id}")
+    suspend fun getQuestionDetail(
+        @Path("id") id: Long
+    ): ApiResult<QuestionDetailResponse>
+
+    @GET("api/questions/{id}/answers")
+    suspend fun getAnswers(
+        @Path("id") id: Long
+    ): ApiResult<List<AnswerResponse>>
 }
