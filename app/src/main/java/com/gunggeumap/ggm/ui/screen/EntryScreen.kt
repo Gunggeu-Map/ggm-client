a137d9ae4c26f6b1b1f3115799f2a2925f3d3f03
ljg0610 <jggy0627@naver.com>
2025-04-19 18:09:25 +0900

    feat: 스플래시 이미지 변경

--- app/src/main/java/com/gunggeumap/ggm/ui/screen/EntryScreen.kt
+++ app/src/main/java/com/gunggeumap/ggm/ui/screen/EntryScreen.kt
@@ -6,7 +6,7 @@ import com.gunggeumap.ggm.ui.MainScreen
 @Composable
 fun EntryScreen() {
     // 하드코딩으로 true 설정 -> 바로 MainScreen 진입
-    var isLoggedIn by remember { mutableStateOf(true) }
+    var isLoggedIn by remember { mutableStateOf(false) }
 
     if (isLoggedIn) {
         // 기존 MainScreen 파라미터 변경 없이 호출
